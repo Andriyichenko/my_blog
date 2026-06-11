d0a4ce9c02f12b54a243b158a75462b00b172df7
Andre <yiran@yahoo.co.jp>
2026-06-11 14:01:01 +0900

    fixed the ito-lemma file

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -6,5 +6,6 @@ export default new Map([
 ["src/content/blog/second-post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsecond-post.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/markdown-style-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/third-post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fthird-post.mdx&astroContentModuleFlag=true")],
-["src/content/blog/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fusing-mdx.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fusing-mdx.mdx&astroContentModuleFlag=true")],
+["src/content/blog/test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftest.mdx&astroContentModuleFlag=true")]]);
 		
