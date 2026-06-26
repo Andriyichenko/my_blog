c9f27df8ebfe8c9b010c94aa5488a900fda9dc0f
Andre <yiran@yahoo.co.jp>
2026-06-26 13:33:27 +0900

    added a new stuff

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,9 +1,9 @@
 
 export default new Map([
-["src/content/blog/itos-lemma.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fitos-lemma.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/markdown-style-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.mdx&astroContentModuleFlag=true")],
-["src/content/blog/method-of-steepest-descent.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmethod-of-steepest-descent.mdx&astroContentModuleFlag=true")],
+["src/content/blog/itos-lemma.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fitos-lemma.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/probability-statistics-foundations.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fprobability-statistics-foundations.mdx&astroContentModuleFlag=true")],
+["src/content/blog/method-of-steepest-descent.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmethod-of-steepest-descent.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/test2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftest2.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fusing-mdx.mdx&astroContentModuleFlag=true")]]);
 		
